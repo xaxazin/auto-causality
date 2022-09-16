c0e69a5d0db6cd2d26c0b2d0b9f5ef034129911a
lekunze <hudson.lekunze@outlook.com>
2022-09-16 17:07:21 +0300

    added SimpleIV baseline estimator with params

--- auto_causality/models/__init__.py
+++ auto_causality/models/__init__.py
@@ -1,6 +1,7 @@
 from auto_causality.models.dummy import (  # noqa: F401
     Dummy,  # noqa: F401
     NewDummy,  # noqa: F401
+    SimpleIV,  # noqa: F401
     OutOfSamplePSWEstimator,  # noqa: F401
     PropensityScoreWeightingEstimator,  # noqa: F401
 )  # noqa: F401
